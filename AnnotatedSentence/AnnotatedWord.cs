1e68966b9c04e683c52f9be9872dd02d228bafd9
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-11-06 11:42:23 +0300

    Updated DependencyParser nuget version and corrected dependencies.

--- AnnotatedSentence/AnnotatedWord.cs
+++ AnnotatedSentence/AnnotatedWord.cs
@@ -1,7 +1,7 @@
 using System.Drawing;
 using System.Globalization;
 using Corpus;
-using DependencyParser;
+using DependencyParser.Universal;
 using Dictionary.Dictionary;
 using MorphologicalAnalysis;
 using NamedEntityRecognition;
